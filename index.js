7e6c8f76d6b6ce837b9e49349ffeeced3a569c92
buenamusicadeproduction-cyber <buenamusicadeproduction@gmail.com>
2025-11-14 14:47:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Inicializa Gemini correctamente
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.example);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash"
 });
